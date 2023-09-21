067ea7b5ff5274e9832673591cba93c7e78398d2
MOHD SAMEER <107679905+SAMEER00112@users.noreply.github.com>
2023-09-22 01:59:51 +0530

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Graph Theory Tutorial',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'ISS Course',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Cloud Computing Tutorial',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
